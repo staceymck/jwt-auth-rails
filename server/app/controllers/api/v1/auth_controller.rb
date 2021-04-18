7a7d25283e16e96ee05d21f79a76011bc173efab
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-17 19:01:52 -0500

    commit tree ff8bf6767157a96a2baa6c14e800298e2d1771df

--- server/app/controllers/api/v1/auth_controller.rb
+++ server/app/controllers/api/v1/auth_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AuthController < ApplicationController
 
   def create # POST /api/v1/login
     @user = User.find_by(username: user_login_params[:username])
-    # @user.authenticate('password')
+    # @user.authenticate('password') // comes from BCrypt gem
     if @user && @user.authenticate(user_login_params[:password])
       @token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
